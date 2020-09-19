838e7f0d883e2759b543b0a101f02c78ba05b039
godOfBugs <godOfBugs@DESKTOP-4J1PEHJ>
2020-09-19 22:14:12 +0300

    Добавил пример использования Elasticsearch на .Net Core

---
+++ elasticExample/Elasticsearch.cs
@@ -0,0 +1,88 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace elasticExample
+{
+    public class Elasticsearch
+    {
+        private Uri _node;
+        private StaticConnectionPool _pool;
+        private ConnectionSettings _settings;
+        private ElasticClient _client;
+        private string defaultIndex;
+
+        public Elasticsearch( string address, string index = null )
+        {
+            defaultIndex = !string.IsNullOrEmpty( index ) ? index : "default";
+
+            _node = new Uri( address );
+            _settings = new ConnectionSettings( _node ).DefaultIndex( defaultIndex );
+            _client = new ElasticClient( _settings );
+        }
+
+        public Elasticsearch( IEnumerable<Uri> addresses )
+        {
+            _pool = new StaticConnectionPool( addresses );
+            _settings = new ConnectionSettings( _pool );
+            _client = new ElasticClient( _settings );
+        }
+
+        /// <summary>
+        /// Индексирование документа
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+        public async Task<Result> IndexBook( BookModel book )
+        {
+            IndexResponse response = await _client.IndexDocumentAsync( book );
+            return response.Result;
+        }
+
+        /// <summary>
+        /// Получение записи по ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public BookModel GetById(int id)
+        {
+            GetResponse<BookModel> response = _client.Get<BookModel>( id );
+            return response.Source;
+        }
+
+        /// <summary>
+        /// Метод поиска
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public async Task<IReadOnlyCollection<BookModel>> SearchBook( string query )
+        {
+            var searchResponse = await _client.SearchAsync<BookModel>( s => s
+            .From( 0 )
+            .Size( 10 )
+            .Query( q => q
+                    .Match( m => m
+                        .Field( f => f.Author )
+                        .Query( query )
+                        )
+                    )
+            );
+
+            return searchResponse.Documents;
+        }
+
+        /// <summary>
+        /// Метод Удаления записи
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<Result> DeleteBook(int id )
+        {
+            Id Id = new Id( id );
+            DeleteResponse response = await _client.DeleteAsync( new DocumentPath<BookModel>( Id ) );
+            return response.Result;
+        }
+    }
+}
---
+++ elasticExample/Form1.cs
@@ -0,0 +1,101 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace elasticExample
+{
+    public partial class Form1 : Form
+    {
+        private Elasticsearch elasticsearch;
+        private int idCounter = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            elasticsearch = new Elasticsearch( "http://localhost:9200", "my_index" );
+
+        }
+
+        private async void addButton_Click( object sender, EventArgs e )
+        {
+            if ( CheckTextBoxes() )
+            {
+                MessageBox.Show( "All fields must be filled" );
+                return;
+            }
+
+            string title = titleTextBox.Text,
+                author = authorTextBox.Text,
+                format = formatTextBox.Text;
+
+            int price = int.Parse( priceTextBox.Text );
+
+            BookModel book = new BookModel { Id = idCounter, Author = author, Title = title, Format = format, Price = price };
+
+            Result result = await elasticsearch.IndexBook( book );
+
+            if ( result == Result.Created )
+            {
+                MessageBox.Show( $"Book with id={idCounter} created", "Success", MessageBoxButtons.OK );
+                idCounter++;
+            }
+            else
+                MessageBox.Show( "Oops...", "Error", MessageBoxButtons.OK );
+        }
+
+        private async void deleteButton_Click( object sender, EventArgs e )
+        {
+            int bookID = int.Parse( deleteBookTextBox.Text );
+            Result result = await elasticsearch.DeleteBook(bookID);
+
+            if ( result == Result.Deleted )
+                MessageBox.Show( $"Book with id={bookID} deleted", "Success", MessageBoxButtons.OK );
+            else
+                MessageBox.Show( "Oops...", "Error", MessageBoxButtons.OK );
+        }
+
+        private void getButton_Click( object sender, EventArgs e )
+        {
+            int id = int.Parse( getByIDTextBox.Text );
+
+            BookModel bookModel = elasticsearch.GetById( id );
+            ShowResult( bookModel );
+        }
+
+        private async void findButton_Click( object sender, EventArgs e )
+        {
+            string query = findTextBox.Text;
+
+            IReadOnlyCollection<BookModel> result = await elasticsearch.SearchBook( query );
+
+            if ( result.Count == 1 )
+                ShowResult( result.First() );
+        }
+
+        private void ShowResult( BookModel book )
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append( $"Title: {book.Title}\r\n" );
+            builder.Append( $"Author: {book.Author}\r\n" );
+            builder.Append( $"Price: {book.Price}\r\n" );
+            builder.Append( $"Format: {book.Format}\r\n" );
+
+            MessageBox.Show( builder.ToString(), "Result", MessageBoxButtons.OK );
+        }
+
+        private bool CheckTextBoxes()
+        {
+            return string.IsNullOrEmpty( titleTextBox.Text ) ||
+                string.IsNullOrEmpty( authorTextBox.Text ) ||
+                string.IsNullOrEmpty( priceTextBox.Text ) ||
+                string.IsNullOrEmpty( formatTextBox.Text );
+        }
+    }
+}
